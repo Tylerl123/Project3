abcdc45af8ae1d6a00e130b2f71af1ed0a0b51bc
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-26 22:17:56 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -102,6 +102,9 @@ function handle_widget_event(e) {
   
        <h3  >  ${first}  </h3> <br>
        `;
+       
+      const.innerHTML = firsts
+      
         if (e.target.dataset.action == "start_again") {
           appState.current_view =  "#intro_view";
           appState.current_model = {
