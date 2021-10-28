bd6c0552f63a2a496dd6a21e986d649b67ad9d6c
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-10-27 21:47:49 -0400

    Update demo.js

--- demo.js
+++ demo.js
@@ -96,7 +96,7 @@ function handle_widget_event(e) {
     // Handle answer event for  text questions.
     if (appState.current_view == "#end_view") {
       let f_name = document.querySelector('#fname').value;
-      alert('fname');
+      alert("f_name");
         if (e.target.dataset.action == "start_again") {
           appState.current_view =  "#intro_view";
           appState.current_model = {
