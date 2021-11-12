09487e8e7aa0d1355d02916dab74f0ee764a435b
Tylerl123 <90160215+Tylerl123@users.noreply.github.com>
2021-11-11 21:57:54 -0500

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,6 +1,6 @@
 // the state of the application
 ///////////////
-const appState = {
+const quizState = {
   current_view: "#intro_view",
   currentGrade: 0,
   current_question: -1,
@@ -128,11 +128,11 @@ function startstopClock(){
 
 document.addEventListener('DOMContentLoaded', () => {
   // Set the state
-  appState.current_view = "#intro_view";
-  appState.current_model = {
+  quizState.current_view = "#intro_view";
+  quizState.current_model = {
     action: "Start"
   }
-  update_view(appState); 
+  update_view(quizState); 
 
   document.querySelector("#widget_view").onclick = (e) => {
     handle_widget_event(e)
@@ -145,85 +145,85 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function handle_widget_event(e) {
 
-  if (appState.current_view == "#intro_view") {
+  if (quizState.current_view == "#intro_view") {
     if (e.target.dataset.action == "Start") {
 
       // Update State (current model + state variables)
-      appState.current_question = 0
+      quizState.current_question = 0
       user = document.querySelector("#name").value;
 
-      appState.current_model = questions[appState.current_question];
+      quizState.current_model = questions[quizState.current_question];
       // process the appState, based on question type update appState.current_view
-      setQuestionView(appState);
+      setQuestionView(quizState);
 
       // Now that the state is updated, update the view.
 
-      update_view(appState);
+      update_view(quizState);
 
-      updateGrade(appState);
+      updateGrade(quizState);
     }
   }
 
   //deals with the image view
   /////////////////
-  if (appState.current_view == "#view_image_selection") {
+  if (quizState.current_view == "#view_image_selection") {
     if (e.target.dataset.action == "answer") {
       user_response = e.target.dataset.answer;
     }
     if (e.target.dataset.action == "submit") {
-      check_answer(user_response, appState.current_model)
+      check_answer(user_response, quizState.current_model)
     }
   }
   //deals with the text input view
   /////////////////
-   if (appState.current_view == "#view_text_input") {
+   if (quizState.current_view == "#view_text_input") {
     if (e.target.dataset.action == "submit") {
-      user_response = document.querySelector(`#${appState.current_model.answerFieldId}`).value;
-      check_answer(user_response, appState.current_model)
+      user_response = document.querySelector(`#${quizState.current_model.answerFieldId}`).value;
+      check_answer(user_response, quizState.current_model)
     }
   }
   //deals with the multiple choice view
   /////////////////
-  if (appState.current_view == "#view_multiple_choice") {
+  if (quizState.current_view == "#view_multiple_choice") {
 
     if (e.target.dataset.action == "answer") {
       user_response = e.target.dataset.answer;
     }
     if (e.target.dataset.action == "submit") {
-      check_answer(user_response, appState.current_model)
+      check_answer(user_response, quizState.current_model)
     }
   }
 
 
   // deals with true/false questions.
   /////////////////
-  if (appState.current_view == "#view_true_false") {
+  if (quizState.current_view == "#view_true_false") {
 
     if (e.target.dataset.action == "answer") {
       user_response = e.target.dataset.answer;
     }
     if (e.target.dataset.action == "submit") {
-      check_answer(user_response, appState.current_model)
+      check_answer(user_response, quizState.current_model)
     }
   }
 
   // deals with the multiple selection.
   ///////////////////
-  if (appState.current_view == "#view_multiple_selection") {
+  if (quizState.current_view == "#view_multiple_selection") {
 
     if (e.target.dataset.action == "submit") {
       var selectValue = document.getElementById("list").value;
       user_response = selectValue
-      check_answer(user_response, appState.current_model)
+      check_answer(user_response, quizState.current_model)
     }
   }
 
   //gives feed back if wrong
  //////////////////// 
 
-  if (appState.current_view == "#feedback_incorrect") {
+  if (quizState.current_view == "#feedback_incorrect") {
     if (e.target.dataset.action == "next") {
-      updateQuestion(appState);
+      updateQuestion(quizState);
     }
   }
 
@@ -231,9 +231,9 @@ function handle_widget_event(e) {
 
   // the final view being diplayed
   ///////////////////
-  if (appState.current_view == "#end_view") {
+  if (quizState.current_view == "#end_view") {
     startstopClock();
-    let grade = ((appState.currentGrade / appState.answered_questions) * 100);
+    let grade = ((quizState.currentGrade / quizState.answered_questions) * 100);
     if (grade > 80) {
       document.getElementById("result").innerHTML = "Your Grade is" + grade + "% <br> " + user + "Congrates You have passes the quiz";
     }
@@ -241,27 +241,27 @@ function handle_widget_event(e) {
       document.getElementById("result").innerHTML = "Your Grade is " + grade + "% <br>" + user + " Sorry but you have failed the quiz, please try again";
     }
     if (e.target.dataset.action == "Start_Again") {
-      appState.current_question = 0
-      appState.currentGrade = 0
-      appState.answered_questions = 0
-      appState.current_model = questions[appState.current_question];
+      quizState.current_question = 0
+      quizState.currentGrade = 0
+      quizState.answered_questions = 0
+      quizState.current_model = questions[quizState.current_question];
      
-      setQuestionView(appState);
+      setQuestionView(quizState);
 
-      update_view(appState);
+      update_view(quizState);
       resetClock();
       startstopClock();
-      updateGrade(appState)
+      updateGrade(quizState)
 
     }
     if (e.target.dataset.action == "intro_page") {
-      appState.currentGrade = 0
-      appState.answered_questions = 0
-      appState.current_view = "#intro_view";
-      appState.current_model = {
+      quizState.currentGrade = 0
+      quizState.answered_questions = 0
+      quizState.current_view = "#intro_view";
+      quizState.current_model = {
         action: "Start"
       }
-      update_view(appState);
+      update_view(quizState);
       resetClock();
     }
   }
@@ -271,62 +271,62 @@ function handle_widget_event(e) {
 //This sets each question type
 //////////////////
 
-function setQuestionView(appState) {
-  if (appState.current_question == -2) {
-    appState.current_view = "#end_view";
+function setQuestionView(quizState) {
+  if (quizState.current_question == -2) {
+    quizState.current_view = "#end_view";
     return
   }
-  if (appState.current_model.questionType == "multiple_choice") {
-    appState.current_view = "#view_multiple_choice";
+  if (quizState.current_model.questionType == "multiple_choice") {
+    quizState.current_view = "#view_multiple_choice";
   }
-  else if (appState.current_model.questionType == "true_false") {
-    appState.current_view = "#view_true_false";
+  else if (quizState.current_model.questionType == "true_false") {
+    quizState.current_view = "#view_true_false";
   }
-  else if (appState.current_model.questionType == "text_input") {
-    appState.current_view = "#view_text_input";
+  else if (quizState.current_model.questionType == "text_input") {
+    quizState.current_view = "#view_text_input";
   }
-  else if (appState.current_model.questionType == "multiple_list") {
-    appState.current_view = "#view_multiple_selection";
+  else if (quizState.current_model.questionType == "multiple_list") {
+    quizState.current_view = "#view_multiple_selection";
   }
-  else if (appState.current_model.questionType == "image_choices") {
-    appState.current_view = "#view_image_selection";
+  else if (quizState.current_model.questionType == "image_choices") {
+    quizState.current_view = "#view_image_selection";
   }
 } 
 
 //This updates the question from the previous
 /////////////////
-function updateQuestion(appState) {
-  if (appState.current_question < questions.length - 1) {
-    appState.current_question = appState.current_question + 1;
-    appState.current_model = questions[appState.current_question];
+function updateQuestion(quizState) {
+  if (quizState.current_question < questions.length - 1) {
+    quizState.current_question = quizState.current_question + 1;
+    quizState.current_model = questions[quizState.current_question];
   }
   else {
-    appState.current_question = -2;
-    appState.current_model = {};
+    quizState.current_question = -2;
+    quizState.current_model = {};
   }
-  setQuestionView(appState);
-  update_view(appState);
+  setQuestionView(quizState);
+  update_view(quizState);
 }
 
 //This checks the user answer if it is correct
 /////////////////
 function check_answer(user_answer, model) {
   if (user_answer == model.correctAnswer) {
-    appState.currentGrade++;
+    quizState.currentGrade++;
     document.querySelector("#widget_view").innerHTML = `
     <div class="container">
     <h2>Correct</h2>
     </div>`
     setTimeout(() => {
-      updateQuestion(appState);
+      updateQuestion(quizState);
     }, 1000);
   }
   else {
-    appState.current_view = "#feedback_incorrect";
-    update_view(appState);
+    quizState.current_view = "#feedback_incorrect";
+    update_view(quizState);
   }
-  appState.answered_questions++;
-  updateGrade(appState);
+  quizState.answered_questions++;
+  updateGrade(quizState);
 }
 
 
@@ -334,16 +334,16 @@ function check_answer(user_answer, model) {
 //this updates the grade from 0
 /////////////////
 
-function updateGrade(appState) {
-  document.querySelector("#comppleted").querySelector("p").innerHTML = `Questions: ${appState.answered_questions}`;
-  var accuracy = Math.floor((appState.currentGrade / appState.answered_questions) * 100);
+function updateGrade(quizState) {
+  document.querySelector("#comppleted").querySelector("p").innerHTML = `Questions: ${quizState.answered_questions}`;
+  var accuracy = Math.floor((quizState.currentGrade / quizState.answered_questions) * 100);
   document.querySelector("#currentGrade").querySelector("p").innerHTML = `Grade: ${accuracy} %`;
 }
 
 //This update the overall view
 ////////////////
-function update_view(appState) {
-  const html_element = render_widget(appState.current_model, appState.current_view)
+function update_view(quizState) {
+  const html_element = render_widget(quizState.current_model, quizState.current_view)
   document.querySelector("#widget_view").innerHTML = html_element;
 }
 
